8d5cac929ca26d7599606ffaf4ec13a97dbe08a3
Dag Odenhall <dag.odenhall@gmail.com>
2010-10-20 18:00:41 +0200

    ZODB models

--- flaskext/zodb.py
+++ flaskext/zodb.py
@@ -7,11 +7,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from contextlib import contextmanager
+from datetime import datetime
+from uuid import uuid4
 
 from werkzeug import cached_property
 from ZODB.DB import DB
 from flask import g
 import transaction
+from persistent import Persistent
+from persistent.list import PersistentList
+from persistent.mapping import PersistentMapping
 
 
 class ZODB(object):
@@ -62,3 +67,98 @@ class ZODB(object):
         finally:
             transaction.commit()
             connection.close()
+
+
+class Factory(object):
+    """Set a :class:`Model` attribute with a callable on instantiation.
+
+    ::
+
+        class Dice(Model):
+            side = Factory(random.randrange, 1, 7)
+
+    ::
+
+        >>> Dice()
+        Dice(side=3)
+        >>> Dice()
+        Dice(side=5)
+
+    """
+
+    def __init__(self, callable, *args, **kwargs):
+        self.callable = callable
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self):
+        return self.callable(*self.args, **self.kwargs)
+
+
+#: UTC timestamp factory
+Timestamp = Factory(datetime.utcnow)
+
+#: UUID4 factory
+UUID4 = Factory(uuid4)
+
+#: Alias for :class:`persistent.list.PersistentList`
+List = PersistentList
+
+#: Alias for :class:`persistent.mapping.PersistentMapping`
+Mapping = PersistentMapping
+
+
+class Model(Persistent):
+    """Convinience model base.
+
+    You can subclass :class:`persistent.Persistent` directly if you prefer,
+    but this base provides some conviniences.
+
+    Set attributes in instantiation::
+
+        >>> Model(title='Hello!')
+        Model(title='Hello!')
+        >> Model(title='Hello!').title
+        'Hello!'
+
+    Declare mutable and dynamic attributes in the class definition::
+
+        class Post(Model):
+            id = UUID4
+            posted_on = Timestamp
+            comments = List
+
+    ::
+
+        >>> Post()
+        Post(id=UUID('c3f043a8-8f1f-4381-89b3-fd1f35265925'),
+             posted_on=datetime.datetime(2010, 10, 20, 15, 42, 34, 138015),
+             comments=[])
+        >>> type(Post().comments)
+        <class 'persistent.list.PersistentList'>
+
+    """
+
+    def __init__(self, **kwargs):
+        mutable = (List, Mapping)
+        callable = (Factory,)
+        for name, value in vars(self.__class__).items():
+            if value in mutable or isinstance(value, callable):
+                setattr(self, name, value())
+
+        for name, value in kwargs.iteritems():
+            try:
+                attribute = getattr(self, name)
+            except AttributeError:
+                attribute = None
+            if isinstance(attribute, List):
+                attribute.extend(value)
+            elif isinstance(attribute, Mapping):
+                attribute.update(value)
+            else:
+                setattr(self, name, value)
+
+    def __repr__(self):
+        attributes = ', '.join('{0}={1!r}'.format(name, value)
+            for (name, value) in vars(self).iteritems())
+        return '{0}({1})'.format(self.__class__.__name__, attributes)
--- stutuz/tests/zodb.py
+++ stutuz/tests/zodb.py
@@ -6,11 +6,48 @@ from __future__ import with_statement
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from datetime import datetime
+from uuid import UUID
+
+from flaskext.zodb import Model, List, Mapping, Timestamp, UUID4
+
 from stutuz.tests import TestBase
 from stutuz import db
 
 
+class TestModel(Model):
+
+    sequence = List
+    mapping = Mapping
+    timestamp = Timestamp
+    id = UUID4
+    something_else = None
+
+
 class ZODB(TestBase):
+
+    def test_model_attributes(self):
+        """Model instantiates List and Mapping and factories"""
+
+        instance = TestModel()
+        self.assert_is_instance(instance.sequence, List)
+        self.assert_is_instance(instance.mapping, Mapping)
+        self.assert_is_instance(instance.timestamp, datetime)
+        self.assert_is_instance(instance.id, UUID)
+        self.assert_is(instance.something_else, None)
+
+    def test_model_kwargs(self):
+        """Model init sets attributes with kwargs"""
+
+        instance = TestModel(sequence=(1, 2, 3), mapping={'foo': 'bar'})
+        self.assert_is_instance(instance.sequence, List)
+        self.assert_is_instance(instance.mapping, Mapping)
+        self.assert_sequence_equal(instance.sequence, (1, 2, 3))
+        self.assert_is(instance.something_else, None)
+
+        instance = TestModel(other='foo', something_else=123)
+        self.assert_equal(instance.other, 'foo')
+        self.assert_equal(instance.something_else, 123)
 
     def test_read(self):
         """Views can read from the database"""
