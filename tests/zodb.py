04344b0a6731d9defd555bf2792bf285271acdcd
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-06 10:01:19 +0200

    Lets not be premature

--- tests/zodb.py
+++ tests/zodb.py
@@ -1,11 +1,3 @@
-#-*- coding:utf-8 -*-
-
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import with_statement
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from datetime import datetime
 from uuid import UUID
 
