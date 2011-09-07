3d733a881ef5f66f1ca5a70d7408e94edeeee9f9
Dag Odenhall <dag.odenhall@gmail.com>
2011-09-07 23:39:07 +0200

    port flaskr example app

---
+++ examples/flaskr/flaskr.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+    Flaskr
+    ~~~~~~
+
+    A microblog example application written as Flask tutorial with
+    Flask and ZODB.  Adapted from the SQLite version in the Flask source
+    distribution.
+
+    :copyright: (c) 2010 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+from __future__ import with_statement
+from sqlite3 import dbapi2 as sqlite3
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+from flaskext.zodb import ZODB, List
+
+# configuration
+ZODB_STORAGE = 'file:///tmp/flaskr.fs'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
+app = Flask(__name__)
+db = ZODB(app)
+
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+@app.before_request
+def set_db_defaults():
+    if 'entries' not in db:
+        db['entries'] = List()
+
+
+@app.route('/')
+def show_entries():
+    return render_template('show_entries.html', entries=db['entries'])
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db['entries'].append(request.form)
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+if __name__ == '__main__':
+    app.run()
