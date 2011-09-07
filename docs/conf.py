72742ba15955926c93c69bfca595bb34582ad9a4
Dag Odenhall <dag.odenhall@gmail.com>
2011-09-08 00:22:14 +0200

    get version for docs from pkg_resources

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,26 @@
 import sys, os
 sys.path.append(os.path.abspath('_themes'))
 
+import pkg_resources
+
+
 project = u'Flask-ZODB'
 copyright = u'2011, Dag Odenhall'
-version = '0.1'
-release = '0.1dev'
+
+release = pkg_resources.get_distribution(project).version
+version = release.split('dev', 1)[0]
+
+
+master_doc = 'index'
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+
 intersphinx_mapping = {
     'http://docs.python.org/': None,
     'http://flask.pocoo.org/docs/': None,
     'http://werkzeug.pocoo.org/docs/': None,
     'http://zodb.readthedocs.org/en/latest/': None,
 }
-
-master_doc = 'index'
 
 html_static_path = ['_static']
 html_theme_path = ['_themes']
